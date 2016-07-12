729ba9d12a2d91cad833cf6b60630ac52a6b596e
rearmlkp <rearmlkp@rearmlkps-MacBook-Pro.local>
2016-07-12 22:04:26 +0700

    Add sticky navigation bar

---
+++ app/assets/javascripts/stickyheader.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rearmlkp on 7/12/16.
+ */
+$(document).ready(function () {
+    var $nav = $('#navigation'),
+        posTop = $nav.position().top;
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > posTop) { $nav.addClass('fixed'); }
+        else { $nav.removeClass('fixed'); }
+    });
+});
